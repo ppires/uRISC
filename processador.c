75b40ac6a11a6c0407017765d7f115af5c30f69b
Pedro Pires <pedro.pires@dito.com.br>
2010-12-04 14:37:40 -0200

    ajustes

--- processador.c
+++ processador.c
@@ -6,8 +6,15 @@ void proc(int argc, char *argv[]){
 	inicializaMemoria(&mem);
 	inicializaALUFlags(&alu_flags);
 	inicializaControleALU(&controle_alu);
-	carregaInstrucoes(&bancoI, argc, argv);
+	carregaInstrucoes(&bancoI, argc, argv); // carrega as instruções do arquivo para a memória de instruções
 	PC = &bancoI->instrucoces[0]; // aponta PC para a primeira instrução
+
+	while(PC != 0x2fff){ // 0x2fff é a instrução de término de processamento
+		IF();
+		ID();
+		EX_MEM();
+		WB();
+	}
 }
 
 void IF(){
